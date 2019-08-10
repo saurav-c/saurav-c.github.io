cd26a2ba7e78e33781ba09d6938644bd30258656
saurav_c <sauravc@berkeley.edu>
2019-08-10 16:33:29 -0700

    Updated website

--- js/custom.js
+++ js/custom.js
@@ -1,137 +1,302 @@
-$(document).ready(function() {
+;(function($){
+    "use strict"
+    var nav_offset_top = $('.header_area').height()+50; 
+    /*-------------------------------------------------------------------------------
+	  Navbar 
+	-------------------------------------------------------------------------------*/
 
-    /* Preloader */
-    $(window).load(function() {
-	    $('#preloader').hide();
-    });
-
-    /* jquery counter */
-	$('.countdown').downCount({
-		date: '06/06/2016 12:00:00',
-        offset: +1
-	});
-
-
-    
-});
-//setTimeoue needed to get full screen canvas
-setTimeout(function() {
-
-function resizeCanvas() {
-  ch = window.innerHeight;
-  cw = window.innerWidth;
-  c.width = cw;
-  c.height = ch;
-};
-
-
-var cw, ch,
-    c = document.getElementById('c'),
-    ctx = c.getContext('2d'),
-    parts = [],
-    globalTick = 0,
-    rand = function(min, max){
-        return Math.floor( (Math.random() * (max - min + 1) ) + min);
+	//* Navbar Fixed  
+    function navbarFixed(){
+        if ( $('.header_area').length ){ 
+            $(window).scroll(function() {
+                var scroll = $(window).scrollTop();   
+                if (scroll >= nav_offset_top ) {
+                    $(".header_area").addClass("navbar_fixed");
+                } else {
+                    $(".header_area").removeClass("navbar_fixed");
+                }
+            });
+        };
     };
-
-
-
-var Part = function(){
-  this.reset();
-};
-
-Part.prototype.reset = function(){
-  this.startRadius = rand(1, 10);
-  // this.startRadius = rand(20, 25);
-  this.radius = this.startRadius;
-  this.x = rand(0, c.width);
-  this.y = rand(0, c.height);
-  this.hue = 210;
-  this.saturation = rand(40, 60);
-  this.lightness = rand(70, 80);
-  this.startAlpha = 0.5;
-  this.alpha = this.startAlpha;
-  this.decayRate = .3;
-  this.startLife = rand(20, 30);
-  // this.startLife = 15;
-  this.life = this.startLife;
-  this.lineWidth = 1;
-}
-
-Part.prototype.update = function(){
-  this.alpha = this.startAlpha * (this.life / this.startLife);
-  this.radius = this.radius+1;
-  this.life -= this.decayRate;
-};
-
-Part.prototype.render = function(){
-  ctx.beginPath();
-  ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-  ctx.fillStyle = ctx.strokeStyle = 'hsla('+this.hue+', '+this.saturation+'%, '+this.lightness+'%, '+this.alpha+')';
-  ctx.lineWidth = this.lineWidth;
-  ctx.fill();
-  // ctx.stroke();
-};
-
-var createParts = function(){
-  parts.push(new Part());
-};
-
-var updateParts = function(){
-  var i = parts.length;
-  while(i--){
-    if (parts[i].life < 0){
-      parts.splice(i, 1)
+    navbarFixed();
+    
+    function testimonialSlider(){
+        if ( $('.testimonial_slider').length ){
+            $('.testimonial_slider').owlCarousel({
+                loop:true,
+                margin: 30,
+                items: 2,
+                nav:false,
+                autoplay: true,
+                dots: true,
+                smartSpeed: 1500,
+                responsiveClass: true,
+                responsive: {
+                    0: {
+                        items: 1,
+                    },
+                    768: {
+                        items: 2,
+                    },
+                }
+            })
+        }
     }
-    parts[i].update();
-  }
-};
+    testimonialSlider();
+    
+    //------- Mailchimp js --------//  
 
-var renderParts = function(){
-  var i = parts.length;
-  while(i--){
-    parts[i].render();
-  }
-};
-
-var clear = function(){
-  ctx.globalCompositeOperation = 'destination-out';
-  ctx.fillStyle = 'hsla(0, 0%, 0%, 1)';
-  ctx.fillRect(0, 0, cw, ch);
-  ctx.globalCompositeOperation = 'source-over';
-};
-
-//Run through the first iterations to get all the parts ready for rendering.
-for (i = 0 ; i < 200 ; i++){
-    if (globalTick % 6 == 0){
-      createParts();
+    function mailChimp(){
+        $('#mc_embed_signup').find('form').ajaxChimp();
     }
-    updateParts();
-    globalTick++;
-}
+    mailChimp();
+    
+    /* ===== Parallax Effect===== */
+	
+	function parallaxEffect() {
+    	$('.bg-parallax').parallax();
+	}
+	parallaxEffect();
+    
+    
+    $('select').niceSelect();
+    $('#datetimepicker11,#datetimepicker1').datetimepicker({
+        daysOfWeekDisabled: [0, 6]
+    });
+    
+     /*---------gallery isotope js-----------*/
+    function galleryMasonry(){
+        if ( $('#gallery').length ){
+            $('#gallery').imagesLoaded( function() {
+              // images have loaded
+                // Activate isotope in container
+                $("#gallery").isotope({
+                    itemSelector: ".gallery_item",
+                    layoutMode: 'masonry',
+                    animationOptions: {
+                        duration: 750,
+                        easing: 'linear'
+                    }
+                });
+            })
+        }
+    }
+    galleryMasonry();
+	
+	/*----------------------------------------------------*/
+    /*  Simple LightBox js
+    /*----------------------------------------------------*/
+    $('.imageGallery1 .light').simpleLightbox();
+    
+    /*----------------------------------------------------*/
+    /*  Google map js
+    /*----------------------------------------------------*/
+    
+    if ( $('#mapBox').length ){
+        var $lat = $('#mapBox').data('lat');
+        var $lon = $('#mapBox').data('lon');
+        var $zoom = $('#mapBox').data('zoom');
+        var $marker = $('#mapBox').data('marker');
+        var $info = $('#mapBox').data('info');
+        var $markerLat = $('#mapBox').data('mlat');
+        var $markerLon = $('#mapBox').data('mlon');
+        var map = new GMaps({
+        el: '#mapBox',
+        lat: $lat,
+        lng: $lon,
+        scrollwheel: false,
+        scaleControl: true,
+        streetViewControl: false,
+        panControl: true,
+        disableDoubleClickZoom: true,
+        mapTypeControl: false,
+        zoom: $zoom,
+            styles: [
+                {
+                    "featureType": "water",
+                    "elementType": "geometry.fill",
+                    "stylers": [
+                        {
+                            "color": "#dcdfe6"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "transit",
+                    "stylers": [
+                        {
+                            "color": "#808080"
+                        },
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.highway",
+                    "elementType": "geometry.stroke",
+                    "stylers": [
+                        {
+                            "visibility": "on"
+                        },
+                        {
+                            "color": "#dcdfe6"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.highway",
+                    "elementType": "geometry.fill",
+                    "stylers": [
+                        {
+                            "color": "#ffffff"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.local",
+                    "elementType": "geometry.fill",
+                    "stylers": [
+                        {
+                            "visibility": "on"
+                        },
+                        {
+                            "color": "#ffffff"
+                        },
+                        {
+                            "weight": 1.8
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.local",
+                    "elementType": "geometry.stroke",
+                    "stylers": [
+                        {
+                            "color": "#d7d7d7"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi",
+                    "elementType": "geometry.fill",
+                    "stylers": [
+                        {
+                            "visibility": "on"
+                        },
+                        {
+                            "color": "#ebebeb"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "administrative",
+                    "elementType": "geometry",
+                    "stylers": [
+                        {
+                            "color": "#a7a7a7"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.arterial",
+                    "elementType": "geometry.fill",
+                    "stylers": [
+                        {
+                            "color": "#ffffff"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.arterial",
+                    "elementType": "geometry.fill",
+                    "stylers": [
+                        {
+                            "color": "#ffffff"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "landscape",
+                    "elementType": "geometry.fill",
+                    "stylers": [
+                        {
+                            "visibility": "on"
+                        },
+                        {
+                            "color": "#efefef"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        {
+                            "color": "#696969"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "administrative",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        {
+                            "visibility": "on"
+                        },
+                        {
+                            "color": "#737373"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi",
+                    "elementType": "labels.icon",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi",
+                    "elementType": "labels",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.arterial",
+                    "elementType": "geometry.stroke",
+                    "stylers": [
+                        {
+                            "color": "#d6d6d6"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road",
+                    "elementType": "labels.icon",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {},
+                {
+                    "featureType": "poi",
+                    "elementType": "geometry.fill",
+                    "stylers": [
+                        {
+                            "color": "#dadada"
+                        }
+                    ]
+                }
+            ]
+        });
+    }
 
-var loop = function(){
-  window.requestAnimFrame(loop, c);
-      clear();
-
-      if (globalTick % 6 == 0){
-        createParts();
-      }
-
-  		updateParts();
-  		renderParts();
-  		globalTick++;
-
-
-};
-
-window.requestAnimFrame=function(){return window.requestAnimationFrame||window.webkitRequestAnimationFrame||window.mozRequestAnimationFrame||window.oRequestAnimationFrame||window.msRequestAnimationFrame||function(a){window.setTimeout(a,1E3/60)}}();
-
-
-resizeCanvas();
-window.onresize = resizeCanvas;
-
-loop();
-
-}, 1);
-
+})(jQuery)
